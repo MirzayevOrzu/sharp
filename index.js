c565a2a758992e25e30ade47d5b28c0a704f2b9e
MirzayevOrzu <mirzayevorzu127@gmail.com>
2022-03-07 21:55:11 +0500

    Read from readableStream, resize width to 300 pixel, write to writableStream

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const app = require('express')()
+const fs = require('fs')
 const multer = require('multer')
 const sharp = require('sharp')
 
+const readableStream = fs.createReadStream('./inputs/different_colored_irises_iris_plant.jpg')
+const writableStream = fs.createWriteStream('./outputs/different_colored_irises_iris_plant.jpg')
 const upload = multer()
 
 app.get('/', (req, res) => {
@@ -10,10 +13,16 @@ app.get('/', (req, res) => {
 
 app.post('/upload', upload.single('file'), async (req, res) => {
 
-    await sharp(req.file.buffer)
-        .resize(400, 400)
-        .toFile(`./outputs/${req.file.originalname}`)
-        
+    const transform = sharp()
+        .resize(300)
+        .on('info', (info) => {
+            console.log(`Image height is ${info.height}`)
+        })
+
+    readableStream
+        .pipe(transform)
+        .pipe(writableStream)
+
     res.send('ok')
 })
 
